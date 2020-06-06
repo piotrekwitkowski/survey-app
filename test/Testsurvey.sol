243799f3aaec067aff55d9624bb82116e0ded378
Carolin Stein <c.stein@campus.tu-berlin.de>
2020-06-06 10:51:49 +0200

    neue testdatei

--- test/Testsurvey.sol
+++ test/Testsurvey.sol
@@ -6,13 +6,13 @@ import "../contracts/Survey.sol";
 
 contract Testsurvey{
  // The address of the adoption contract to be tested
- Survey survey1 = Survey(DeployedAddresses.Survey());
+ Survey survey1 = Survey(DeployedAddresses.Survey("89","89",4));
 
 // Testing the get_URL function
 function test_owner_gets_URL() public {
  string URL_gotten = survey1.get_URL();
 
-  Assert.equal("123", URL_gotten, "Should match.");
+  Assert.equal("89", URL_gotten, "Should match.");
 }
 
 }
