f9ef985ab9f4fc5ed50bbb94f3d0da7d76d11bc4
Carolin Stein <c.stein@campus.tu-berlin.de>
2020-06-08 09:06:16 +0200

    Survey contract modified, includs arrays

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -1,44 +1,58 @@
 pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
 
 contract Survey {
     address public _owner;
     string public _surveyname;
-    string private _surveyURL;
+    string private _surveyURL = "123";
     uint private _amountparticipants;
     enum State { Created, Open, Ended }
     State public state;
-    
+    string[] survey_questionnair;
+    string[][] survey_storage;
+
     constructor(string memory name, string memory URL, uint participants) public{
         _owner = msg.sender;
-        _surveyname = name; 
+        _surveyname = name;
         _surveyURL = URL;
         _amountparticipants = participants;
         state = State.Created;
-        
+
     }
-    
-    function get_URL() public view returns (string memory) {
+function get_URL() public view returns (string memory) {
         return _surveyURL;
     }
-    
+
     function get_amountparticipants() public view  returns (uint) {
         return _amountparticipants;
     }
-    
+
     modifier _editor{
         require(msg.sender == _owner);
         _;
         // can add other contracts that need to do something
     }
-    
+
     function set_amountparticipants(uint new_participantnumber) public  _editor{
         require(state == State.Created);
         _amountparticipants = new_participantnumber;
     }
     
-    function InitSurvey() public  _editor{
+     function PrepareSurvey(string[] memory questions ) public _editor returns(string[] memory)  {
+        require(state == State.Created);
+                for (uint i = 0; i < _amountparticipants; i++) {
+                         survey_questionnair[i] = questions[i];
+                }
+                return survey_questionnair;
+        }
+
+   function InitSurvey(uint _amountquestions) public _editor returns(string[][] memory) {
+        require (state == State.Created);
         state = State.Open;
-        //Links need to be created, other stuff needs to be done :)
-    }
+        survey_storage[_amountparticipants][_amountquestions];
+        return survey_storage;
+
+        }
+
+
 }
-    
--- test/Testsurvey.sol
+++ test/Testsurvey.sol
@@ -6,13 +6,19 @@ import "../contracts/Survey.sol";
 
 contract Testsurvey{
  // The address of the adoption contract to be tested
- Survey survey1 = Survey(DeployedAddresses.Survey("89","89",4));
+ Survey survey1 = Survey(DeployedAddresses.Survey());
+ uint test_amount = 9;
+// Testing function set_amount_participants
+function test_set_amount_pat() public {
+survey1.set_amountparticipants(test_amount);
+uint amountpat = survey1.get_amountparticipants();
+Assert.equal (test_amount, amountpat, "Should match");
+}
 
 // Testing the get_URL function
-function test_owner_gets_URL() public {
- string URL_gotten = survey1.get_URL();
-
-  Assert.equal("89", URL_gotten, "Should match.");
-}
+//function test_owner_gets_URL() public {
+ //string memory URL_gotten = survey1.get_URL();
+ // Assert.equal("123", URL_gotten, "Should match.");
+//}
 
 }
