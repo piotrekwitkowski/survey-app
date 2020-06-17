6a17cc9d6342743ffa13c0a8a878d9fa97e0ac66
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-06-17 11:29:05 +0200

    format: some vscode autoformatting

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -1,78 +1,94 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract Survey {
     address public _owner;
     string public _surveyname;
     string private _surveyURL = "123";
-    uint private _amountparticipants;
-    uint private _actual_participants = 0;
-    enum State { Created, Open, Ended }
+    uint256 private _amountparticipants;
+    uint256 private _actual_participants = 0;
+    enum State {Created, Open, Ended}
     State public state;
     string[] public survey_questionnair;
     string[][] private survey_storage;
-    uint private _amountquestions; 
+    uint256 private _amountquestions;
 
-    constructor(string memory name, string memory URL, uint participants) public{
+    constructor(
+        string memory name,
+        string memory URL,
+        uint256 participants
+    ) public {
         _owner = msg.sender;
         _surveyname = name;
         _surveyURL = URL;
         _amountparticipants = participants;
         state = State.Created;
-
     }
-function get_URL() public view returns (string memory) {
+
+    function get_URL() public view returns (string memory) {
         return _surveyURL;
     }
 
-    function get_amountparticipants() public view  returns (uint) {
+    function get_amountparticipants() public view returns (uint256) {
         return _amountparticipants;
     }
 
-    modifier _editor{
+    modifier _editor {
         require(msg.sender == _owner);
         _;
         // can add other contracts that need to do something
     }
 
-    function set_amountparticipants(uint new_participantnumber) public  _editor{
+    function set_amountparticipants(uint256 new_participantnumber)
+        public
+        _editor
+    {
         require(state == State.Created);
         _amountparticipants = new_participantnumber;
     }
-    
-     function PrepareSurvey(string[] memory questions, uint amountquestions ) public _editor returns(string[] memory)  {
-        require(state == State.Created);
-        _amountquestions = amountquestions-1;
-                for (uint i = 0; i < _amountquestions; i++) {
-                         survey_questionnair[i] = questions[i];
-                }
-                return survey_questionnair;
-        }
 
-   function InitSurvey() public _editor returns(string[][] memory) {
-        require (state == State.Created);
+    function PrepareSurvey(string[] memory questions, uint256 amountquestions)
+        public
+        _editor
+        returns (string[] memory)
+    {
+        require(state == State.Created);
+        _amountquestions = amountquestions - 1;
+        for (uint256 i = 0; i < _amountquestions; i++) {
+            survey_questionnair[i] = questions[i];
+        }
+        return survey_questionnair;
+    }
+
+    function InitSurvey() public _editor returns (string[][] memory) {
+        require(state == State.Created);
         state = State.Open;
         survey_storage[_amountparticipants][_amountquestions];
         return survey_storage;
+    }
 
-        }
     function participate(string[] memory answers) public {
-        require (state == State.Open && (_actual_participants < _amountparticipants) && (msg.sender != _owner));
-        for (uint i = 0; i <= _amountquestions; i++ ) {
-           survey_storage[_actual_participants][i] =  answers[i];
+        require(
+            state == State.Open &&
+                (_actual_participants < _amountparticipants) &&
+                (msg.sender != _owner)
+        );
+        for (uint256 i = 0; i <= _amountquestions; i++) {
+            survey_storage[_actual_participants][i] = answers[i];
         }
-        _actual_participants ++; 
+        _actual_participants++;
         if (_actual_participants == _amountparticipants) {
-            state = State.Ended; 
+            state = State.Ended;
         }
     }
-    
-    function retrieve_results () public view _editor returns(string[][] memory) {
-        require (state == State.Ended);
+
+    function retrieve_results()
+        public
+        view
+        _editor
+        returns (string[][] memory)
+    {
+        require(state == State.Ended);
         return survey_storage;
     }
-
 }
-
-
-
--- test/Testsurvey.sol
+++ test/Testsurvey.sol
@@ -4,21 +4,22 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Survey.sol";
 
-contract Testsurvey{
- // The address of the adoption contract to be tested
- Survey survey1 = Survey(DeployedAddresses.Survey());
- uint test_amount = 9;
-// Testing function set_amount_participants
-function test_set_amount_pat() public {
-survey1.set_amountparticipants(test_amount);
-uint amountpat = survey1.get_amountparticipants();
-Assert.equal (test_amount, amountpat, "Should match");
-}
+contract Testsurvey {
+    // The address of the adoption contract to be tested
+    Survey survey = Survey(DeployedAddresses.Survey());
 
-// Testing the get_URL function
-//function test_owner_gets_URL() public {
- //string memory URL_gotten = survey1.get_URL();
- // Assert.equal("123", URL_gotten, "Should match.");
-//}
+    uint256 test_amount = 9;
 
+    // Testing function set_amount_participants
+    function test_set_amount_pat() public {
+        survey.set_amountparticipants(test_amount);
+        uint256 amountpat = survey.get_amountparticipants();
+        Assert.equal(test_amount, amountpat, "Should match");
+    }
+
+    // Testing the get_URL function
+    function test_owner_gets_URL() public {
+        string memory URL_gotten = survey.get_URL();
+        Assert.equal("123", URL_gotten, "Should match.");
+    }
 }
