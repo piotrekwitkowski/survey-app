4994b7918ec34096ab1b142ad864b377a4f2dbbe
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-06-21 15:33:23 +0200

    fix: formatting

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -6,7 +6,7 @@ contract Survey {
     address public _owner;
     string public _surveyname;
     string private _surveyURL;
-    uint256 private _amountparticipants=3;
+    uint256 private _amountparticipants = 3;
     uint256 private _actual_participants = 0;
     enum State {CREATED, OPEN, ENDED}
     State public state;
@@ -30,36 +30,37 @@ contract Survey {
         return _surveyURL;
     }
 
- //   function get_amountparticipants() public view returns (uint256) {
-  //      return _amountparticipants;
-  //  }
+    //   function get_amountparticipants() public view returns (uint256) {
+    //      return _amountparticipants;
+    //  }
 
     modifier _editor {
         require(msg.sender == _owner, "Sender is not the owner");
         _;
         // can add other contracts that need to do something
     }
+
     //curently does not make sense with hot fix
     //function set_amountparticipants(uint256 new_participantnumber)
     //    public
     //{
-     //   require(state == State.CREATED, "State is not CREATED");
-      //  _amountparticipants = new_participantnumber;
+    //   require(state == State.CREATED, "State is not CREATED");
+    //  _amountparticipants = new_participantnumber;
     //}
 
     //curently does not make sense with hot fix
-   // function set_amountquestions(uint256 amount) public
-   // {
+    // function set_amountquestions(uint256 amount) public
+    // {
     //    require(state == State.CREATED, "State is not CREATED");
     //    _amountquestions = amount;
-  //  } 
-    
-  //  function get_amountquestions() 
-  //  public
-  //  returns(uint amount) 
-  //  {
-  //      return _amountquestions;
-  //  }
+    //  }
+
+    //  function get_amountquestions()
+    //  public
+    //  returns(uint amount)
+    //  {
+    //      return _amountquestions;
+    //  }
 
     function PrepareSurvey(string[3] memory questions)
         public
@@ -77,7 +78,10 @@ contract Survey {
         state = State.OPEN;
     }
 
-    function participate(string[3] memory answers) public returns(string[3][3] memory) {
+    function participate(string[3] memory answers)
+        public
+        returns (string[3][3] memory)
+    {
         require(state == State.OPEN, "State is not OPEN");
         require(msg.sender != _owner, "Sender is not the owner");
         require(
@@ -95,9 +99,8 @@ contract Survey {
         return survey_storage;
     }
 
-    function retrieve_results() public returns(string[3][3] memory) {
+    function retrieve_results() public returns (string[3][3] memory) {
         require(state == State.ENDED, "State is not ENDED");
-    return survey_storage;
+        return survey_storage;
     }
 }
-
