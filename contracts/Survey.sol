042d1cc4af237c6af9eb6dd80fe14e41e684a685
kbalitzki <kbalitzki@avm.de>
2020-06-23 10:40:34 +0200

    comments added

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -6,13 +6,13 @@ contract Survey {
     address public _owner;
     string public _surveyname;
     string private _surveyURL;
-    uint256 private _amountparticipants = 3;
+    uint256 private _amountparticipants;
     uint256 private _actual_participants = 0;
     enum State {CREATED, OPEN, ENDED}
     State public state;
-    string[3] public survey_questionnair;
-    string[3][3] public survey_storage;
-    uint256 private _amountquestions = 3;
+    string[] public survey_questionnair;
+    string[][] private survey_storage;
+    uint256 private _amountquestions;
 
     constructor(
         string memory name,
@@ -26,62 +26,70 @@ contract Survey {
         state = State.CREATED;
     }
 
+    /* 
+    Getter functions
+    */
     function get_URL() public view returns (string memory) {
         return _surveyURL;
     }
 
-    //   function get_amountparticipants() public view returns (uint256) {
-    //      return _amountparticipants;
-    //  }
+    function get_amountparticipants() public view returns (uint256) {
+        return _amountparticipants;
+    }
 
+    /*
+    Setter functions
+    */
+    function set_amountparticipants(uint256 new_participantnumber)
+        public
+    {
+        require(state == State.CREATED, "State is not CREATED");
+        _amountparticipants = new_participantnumber;
+    }
+
+    // todo
     modifier _editor {
         require(msg.sender == _owner, "Sender is not the owner");
         _;
         // can add other contracts that need to do something
     }
 
-    //curently does not make sense with hot fix
-    //function set_amountparticipants(uint256 new_participantnumber)
-    //    public
-    //{
-    //   require(state == State.CREATED, "State is not CREATED");
-    //  _amountparticipants = new_participantnumber;
-    //}
-
-    //curently does not make sense with hot fix
-    // function set_amountquestions(uint256 amount) public
-    // {
-    //    require(state == State.CREATED, "State is not CREATED");
-    //    _amountquestions = amount;
-    //  }
-
-    //  function get_amountquestions()
-    //  public
-    //  returns(uint amount)
-    //  {
-    //      return _amountquestions;
-    //  }
-
-    function PrepareSurvey(string[3] memory questions)
+    /*
+    If a survey is created ??
+    */
+    function PrepareSurvey(string[] memory questions, uint256 amountquestions)
         public
-        returns (string[3] memory)
+        _editor
+        returns (string[] memory)
     {
         require(state == State.CREATED, "State is not CREATED");
+        // ??
+        _amountquestions = amountquestions - 1;
+        // transferd survey questions into local var
         for (uint256 i = 0; i < _amountquestions; i++) {
             survey_questionnair[i] = questions[i];
         }
         return survey_questionnair;
     }
 
-    function InitSurvey() public {
+    /* 
+    Opens a survey after creation
+    param ???
+    */
+    function InitSurvey() public _editor returns (string[][] memory) {
         require(state == State.CREATED, "State is not CREATED");
         state = State.OPEN;
+        survey_storage[_amountparticipants][_amountquestions];
+        return survey_storage;
     }
 
-    function participate(string[3] memory answers)
-        public
-        returns (string[3][3] memory)
-    {
+    // todo: extend with participation contract
+
+    /*
+    Adds a participant to the current participants if the survey hasend ended yet
+    param answers: array of answers to the survey
+    */
+    function participate(string[] memory answers) public {
         require(state == State.OPEN, "State is not OPEN");
         require(msg.sender != _owner, "Sender is not the owner");
         require(
@@ -89,17 +97,24 @@ contract Survey {
             "too many participants"
         );
 
-        for (uint256 i = 0; i < _amountquestions; i++) {
+        for (uint256 i = 0; i <= _amountquestions; i++) {
             survey_storage[_actual_participants][i] = answers[i];
         }
         _actual_participants++;
         if (_actual_participants == _amountparticipants) {
             state = State.ENDED;
         }
-        return survey_storage;
     }
 
-    function retrieve_results() public view returns (string[3][3] memory) {
+    /*
+    Returns results if survey has ended
+    */
+    function retrieve_results()
+        public
+        view
+        _editor
+        returns (string[][] memory)
+    {
         require(state == State.ENDED, "State is not ENDED");
         return survey_storage;
     }
