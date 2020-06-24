d4c4767bb0a3712900546acb86c9af5bbbec9ea7
Carolin Stein <c.stein@campus.tu-berlin.de>
2020-06-24 14:26:12 +0200

    Deploy changed, now deploying master contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
-var Survey = artifacts.require("Survey");
-var Participate = artifacts.require("Participate");
+// var Survey = artifacts.require("Survey");
+//var Participate = artifacts.require("Participate");
+var Master = artifacts.require("Master");
 
 module.exports = function(deployer) {
-  deployer.deploy(Survey);
+  deployer.deploy(Master);
   // deployer.deploy(Survey, "Surveyname2", "SurveyURL2", 3);
   // deployer.deploy(Survey, "Surveyname3", "SurveyURL3", 3);
-  deployer.deploy(Participate, "hello", "hello");
+  //deployer.deploy(Participate, "hello", "hello");
 };
