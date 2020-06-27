0517ec822c2931b9c3ff42b76e65b5903698ebc7
Piotr Witkowski <piotrekwitkowski1@gmail.com>
2020-06-28 01:12:40 +0200

    style: autoformat

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 pragma experimental ABIEncoderV2;
+
 //import "./Participate.sol";
 
 contract Survey {
     enum State {CREATED, OPEN, ENDED}
+
     address private _owner;
     string[] private _questions;
     string[][] private _answers;
@@ -55,14 +57,14 @@ contract Survey {
         address caller
     ) public requireStateOpen {
         //Participate participant = new Participate(this, caller);
-            participantsList.push(caller);
-            _answers[_participants] = answers;
-            _participants++;
+        participantsList.push(caller);
+        _answers[_participants] = answers;
+        _participants++;
 
-            if (_participants == _maxParticipants) {
-                state = State.ENDED;
-                finish = true;
-            }
+        if (_participants == _maxParticipants) {
+            state = State.ENDED;
+            finish = true;
+        }
     }
 
     function getAnswers() public view returns (string[][] memory) {
@@ -70,8 +72,8 @@ contract Survey {
         return _answers;
     }
 
-    function getParticipantList() public view returns (address[] memory ) {
-        address[]memory List = participantsList;
+    function getParticipantList() public view returns (address[] memory) {
+        address[] memory List = participantsList;
         return List;
     }
 }
